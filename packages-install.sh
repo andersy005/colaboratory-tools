06e0d301574706a70f70ea4e46282dbac127b5b3
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-04-10 19:36:48 -0500

    fix could not open file error

--- packages-install.sh
+++ packages-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing packages"
-pip install -r -q requirements.txt --no-index --find-links file:///tmp/packages
+pip install -r requirements.txt --quiet --no-index --find-links file:///tmp/packages
 echo "Finished installing packages"
